54f1b5ad2b190b3a3eaf1ce95e6a01e80b89855c
Samaybyte <samisole1996@gmail.com>
2022-04-22 13:37:40 -0700

    updated

--- print_string.c
+++ print_string.c
@@ -10,16 +10,16 @@
 
 int print_str(va_list arg)
 {
-int i;
-char *str = va_arg(arg, char*);
-
-if (str == NULL)
-	str = "(null)";
-else if (*str == '\0')
-	return (-1);
-
-for (i = 0; str[i]; i++)
-	_putchar(str[i]);
-
-return (i);
+	int i;
+	char *str = va_arg(arg, char*);
+	
+	if (str == NULL)
+		str = "(null)";
+	else if (*str == '\0')
+		return (-1);
+	
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+	
+	return (i);
 }
