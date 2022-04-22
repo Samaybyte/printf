76c502b052141f1a402d26bf3d35be91c65232e1
Samuel Solomon <85625481+Samaybyte@users.noreply.github.com>
2022-04-22 22:32:25 +0300

    Update main.h

--- main.h
+++ main.h
@@ -11,7 +11,8 @@ int _printf(const char *format, ...);
 int printIdentifiers(char next, va_list arg);
 int print_char(va_list arg);
 int print_str(va_list arg);
-int print_percent(void);
+int percentage(__attribute__((unused))va_list param);
+/*int print_percent(void);*/
 void print_binary(unsigned int n, unsigned int* printed);
 int print_unsignedToBinary(va_list arg);
 int print_oct(va_list arg);
